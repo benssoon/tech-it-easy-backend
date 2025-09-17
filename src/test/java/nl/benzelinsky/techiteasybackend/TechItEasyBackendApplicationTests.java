40f26155e614f010f37127aba177a72245d5e49c
ben <benjamin.zelinsky@gmail.com>
2025-09-17 11:16:56 +0200

    Set up project.

---
+++ src/test/java/nl/benzelinsky/techiteasybackend/TechItEasyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.benzelinsky.techiteasybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechItEasyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
