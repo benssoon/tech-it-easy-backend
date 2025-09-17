3d12cfdd71f89d6b942610f329eb8e5bc815eed6
ben <benjamin.zelinsky@gmail.com>
2025-09-17 12:14:15 +0200

    Add simple GET, POST, PUT, and DELETE methods.

--- src/main/java/nl/benzelinsky/techiteasybackend/controllers/TelevisionsController.java
+++ src/main/java/nl/benzelinsky/techiteasybackend/controllers/TelevisionsController.java
@@ -1,14 +1,42 @@
 package nl.benzelinsky.techiteasybackend.controllers;
 
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/tvs")
 public class TelevisionsController {
 
     //GET request all tvs
+    @GetMapping
+    public ResponseEntity<String> getAllTelevisions() {
+        return ResponseEntity.ok("all televisions");
+    }
 
     //GET request 1 tv
+    @GetMapping("/{id}")
+    public ResponseEntity<String> getTelevision() {
+        return ResponseEntity.ok("television");
+    }
 
     //POST request 1 tv
+    @PostMapping
+    public ResponseEntity<Object> createTelevision() {
+        //return new ResponseEntity<>("created television", HttpStatus.CREATED);
+        return ResponseEntity.created(null).body("created television");
+    }
 
     //PUT request 1 tv
+    @PutMapping
+    public ResponseEntity<String> updateTelevision() {
+        return ResponseEntity.ok("updated television");
+    }
 
     //DELETE request 1 tv
+    @DeleteMapping
+    public ResponseEntity<Object> deleteTelevison() {
+        return ResponseEntity.ok("deleted television");
+    }
 }
