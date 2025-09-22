c6e8ce3b282499fabb40ae8b47e98bffce16ef26
ben <benjamin.zelinsky@gmail.com>
2025-09-22 14:15:19 +0200

    Add Television class so that real data can be sent, instead of just hard coded strings.

--- src/main/java/nl/benzelinsky/techiteasybackend/controllers/TelevisionsController.java
+++ src/main/java/nl/benzelinsky/techiteasybackend/controllers/TelevisionsController.java
@@ -1,31 +1,45 @@
 package nl.benzelinsky.techiteasybackend.controllers;
 
+import nl.benzelinsky.techiteasybackend.model.Television;
 import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/tvs")
 public class TelevisionsController {
 
+    //temp database
+    Map<Integer, Television> televisionMap = new HashMap<Integer, Television>();
+
+    //POST request 1 tv
+    @PostMapping
+    public ResponseEntity<Object> createTelevision(@RequestBody Television tv) {
+        this.televisionMap.put(tv.getId(), tv);
+
+        //return new ResponseEntity<>(tv, HttpStatus.CREATED);
+        return ResponseEntity.created(null).body(tv);
+    }
+
     //GET request all tvs
     @GetMapping
-    public ResponseEntity<String> getAllTelevisions() {
-        return ResponseEntity.ok("all televisions");
+    public ResponseEntity<Map<Integer, Television>> getAllTelevisions() {
+        return ResponseEntity.ok(this.televisionMap);
     }
 
     //GET request 1 tv
     @GetMapping("/{id}")
-    public ResponseEntity<String> getTelevision() {
-        return ResponseEntity.ok("television");
-    }
-
-    //POST request 1 tv
-    @PostMapping
-    public ResponseEntity<Object> createTelevision() {
-        //return new ResponseEntity<>("created television", HttpStatus.CREATED);
-        return ResponseEntity.created(null).body("created television");
+    public ResponseEntity<Object> getTelevisionById(@PathVariable int id) {
+        if (this.televisionMap.containsKey(id)) {
+            return ResponseEntity.ok(this.televisionMap.get(id));
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     //PUT request 1 tv
